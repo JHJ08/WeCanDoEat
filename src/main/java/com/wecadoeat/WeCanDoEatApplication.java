f3a87e696c952284f11dbf56f4a9d754489c25c8
JHJ08 <161445093+JHJ08@users.noreply.github.com>
2024-10-08 08:42:35 -0700

    chore: 스프링 부트 프로젝트 생성 및 관련 설정 초기화 - .gitignore 설정 - web context root path 설정 - 포트번호 설정

---
+++ src/main/java/com/wecadoeat/WeCanDoEatApplication.java
@@ -0,0 +1,13 @@
+package com.wecadoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeCanDoEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeCanDoEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wecadoeat/WeCanDoEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecadoeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeCanDoEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
